4018d1b4449b5830405e96d9eded95e3326abd54
conradu <conrado.salomao@gmail.com>
2022-06-04 04:28:50 -0300

    Login, Banco de dados e Policies finalizados e funcionais

--- config/routes.js
+++ config/routes.js
@@ -1,28 +1,50 @@
 module.exports.routes = {
 
-  '/': { view: 'pages/login' },
+  '/': function (req, res) {
+    res.redirect('/login');
+  },
+
+  'GET /login': {
+    policy: 'naologado',
+    view: 'pages/login'
+  },
 
   'POST /login': {
     controller: 'usuario',
     action: 'login'
   },
 
+  'GET /logout': {
+    policy : 'logado',
+    controller: 'usuario',
+    action: 'logout'
+  },
+
+  '/home': {
+    policy : 'logado',
+    view: 'pages/home'
+  },
+
   'GET /user/criar': {
+    policy : 'admin',
     controller: 'usuario',
     action: 'insertTeste'
   },
 
   'GET /user/retorna': {
+    policy : 'admin',
     controller: 'usuario',
     action: 'retornaUser'
   },
 
   'GET /adm/criarADM': {
+    policy : 'admin',
     controller: 'administrador',
     action: 'criarADM'
   },
 
   'GET /adm/retornaADM': {
+    policy : 'admin',
     controller: 'administrador',
     action: 'retornaADM'
   }
