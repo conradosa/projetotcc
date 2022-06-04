28604f143d3b0b46493ea1671c7b45c147a9db51
conradu <conrado.salomao@gmail.com>
2022-06-04 04:16:01 -0300

    Mudanças

--- config/routes.js
+++ config/routes.js
@@ -1,50 +1,28 @@
 module.exports.routes = {
 
-  '/': function (req, res) {
-    res.redirect('/login');
-  },
-
-  'GET /login': {
-    policy: 'naologado',
-    view: 'pages/login'
-  },
+  '/': { view: 'pages/login' },
 
   'POST /login': {
     controller: 'usuario',
     action: 'login'
   },
 
-  'GET /logout': {
-    policy : 'logado',
-    controller: 'usuario',
-    action: 'logout'
-  },
-
-  '/home': {
-    policy : 'logado',
-    view: 'pages/home'
-  },
-
   'GET /user/criar': {
-    policy : 'admin',
     controller: 'usuario',
     action: 'insertTeste'
   },
 
   'GET /user/retorna': {
-    policy : 'admin',
     controller: 'usuario',
     action: 'retornaUser'
   },
 
   'GET /adm/criarADM': {
-    policy : 'admin',
     controller: 'administrador',
     action: 'criarADM'
   },
 
   'GET /adm/retornaADM': {
-    policy : 'admin',
     controller: 'administrador',
     action: 'retornaADM'
   }
